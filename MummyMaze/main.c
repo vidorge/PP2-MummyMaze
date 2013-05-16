e4b55abeae2dcb58f0cfc814e8c32eae7887a436
vidorge <vidorge@gmail.com>
2013-05-16 19:00:21 +0200

    Added simple matrix functions

--- MummyMaze/main.c
+++ MummyMaze/main.c
@@ -2,43 +2,19 @@
 #include <stdlib.h>
 
 #include "stack.h"
-
-#define MAX 15
-
+#include "maze_create.h"
 
 
 
-
-
-
-void main()
+int main()
 {
-	int a[MAX][MAX];
-	int exit = MAX / 2;
-	int i, j;
-	lElem *head;
+	int **matrix;
+	
+	//i'm a sad sad main :(
 
-	head = NULL;
+	matrix = initMatrix(DIM);
+	printDebugMatrix(matrix, DIM);
+	
 
-	//printf("LOL\n");
-
-	/*
-
-	for (i=0; i<MAX; i++)
-		for(j=0; j<MAX; j++)
-			a[i][j] = 0;
-
-	a[exit][29] = 1;
-
-	for (i=0; i<MAX; i++, printf("\n"))
-		for(j=0; j<MAX; j++)
-			printf("%d", a[i][j]);
-
-
-	*/
-
-
-
-
-	//additional edit
+	return 0;
 }
--- MummyMaze/maze_create.h
+++ MummyMaze/maze_create.h
@@ -1,30 +1,34 @@
 ﻿#ifndef _maze_create_h
 #define _maze_create_h
 
+#define DIM 15
+
+/**
+*	@desc	Initialize a matrix
+*	@param	int dimension - dimension of matrix
+*	@return	int ** - double pointer to matrix
+*	@author Vidor Gencel
+*/
+int **initMatrix(int dimension);
 
 
+/**
+*	@desc	Prints debug-formated matrix 1/0
+*	@param	int **matrix - double pointer to matrix
+*	@param	int dimension - dimension of matrix
+*	@author Vidor Gencel
+*/
+int **printDebugMatrix(int **matrix, int dimension);
 
+
+/**
+*	@desc	See if a cell from game board has unvisited neighbours
+*	@param	int **matrix - double pointer to game board matrix
+*	@return	1 - has unvisited, 0 - doesn't
+*	@author Vidor Gencel
+*/
+//int hasUnvisitedNeighbour(int **matrix);
 
 
 
 #endif
-
-
-/*
-
-Depth-First Search:
-
-1. Izabrati ćeliju i označiti je kao izlaz,
-2. Označiti trenutnu ćeliju kao „posećena“,
-3. Ako ćelija ima neposećenih komšija:
-	1) Slučajno odabrati jednog od komšija,
-	2) Staviti trenutnu ćeliju na stek,
-	3) Skloniti zid između trenutne ćelije i izabrane ćelije,
-	4) Izabranu ćeliju napraviti trenutnom ćelijom,
-	5) Rekurzivno pozivati ovu funkciju, 
-
-4. Ako nema:
-	1) Skinuti poslednju ćeliju sa steka,
-	2) Vratiti se na prethodno pozivanje ove funkcije. 
-
-*/
