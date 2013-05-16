090b882660245bb973c812128c3a40913603a592
Di-mi <ixydimitrov@gmail.com>
2013-05-17 01:14:38 +0200

    GUI HUB

---
+++ MummyMaze/maze_create.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "maze_create.h"
+
+
+int **initMatrix(int dimension)
+{
+	int **a, i, j, r, c;
+
+	a = (int**) malloc( dimension * sizeof(int*) );
+
+	for (i=0; i<dimension; i++)
+	{
+		a[i]  = malloc( dimension * sizeof(int) );
+		for (j=0; j<dimension; j++)
+			a[i][j] = 1;
+	}
+
+	srand( time(NULL) );
+
+	c = rand() % dimension;
+	while( c % 2 == 0 )
+		c = rand() % dimension;
+
+	r = rand() % dimension;
+	while( r % 2 == 0 )
+		r = rand() % dimension;
+
+	a[r][c] = 0;
+
+
+	dfs(a, r, c, dimension);
+
+	return a;
+
+}
+
+
+void dfs(int **a, int r, int c, int dimension)
+{
+	int randDir[] = {3, 4, 2, 1}; // Must randomize
+	int i;
+
+	for (i = 0; i < 4; i++) {
+ 
+         switch(randDir[i]){
+         case 1: // Up
+             if (r - 2 <= 0) continue;
+             if (a[r - 2][c] != 0) {
+                 a[r-2][c] = 0;
+                 a[r-1][c] = 0;
+                 dfs(a, r - 2, c, dimension);
+             }
+             break;
+
+         case 2: // Right
+             if (c + 2 >= dimension - 1) continue;
+             if (a[r][c + 2] != 0) {
+                 a[r][c + 2] = 0;
+                 a[r][c + 1] = 0;
+                 dfs(a, r, c + 2, dimension);
+             }
+             break;
+
+         case 3: // Down
+             if (r + 2 >= dimension - 1)  continue;
+             if (a[r + 2][c] != 0) {
+                 a[r+2][c] = 0;
+                 a[r+1][c] = 0;
+                 dfs(a, r + 2, c, dimension);
+             }
+             break;
+
+         case 4: // Left
+             if (c - 2 <= 0) continue;
+             if (a[r][c - 2] != 0) {
+                 a[r][c - 2] = 0;
+                 a[r][c - 1] = 0;
+                 dfs(a, r, c - 2, dimension);
+             }
+             break;
+
+         }
+     }
+
+}
+
+
+void printDebugMatrix(int **matrix, int dimension)
+{
+	int i, j;
+
+	for (i=0; i<dimension; i++, printf("\n") )
+		for (j=0; j<dimension; j++)
+			printf("%d", matrix[i][j]);
+
+}
+
+/*
+int hasUnvisitedNeighbour(int **matrix)
+{
+
+
+
+}
+*/
--- MummyMaze/maze_gui.c
+++ MummyMaze/maze_gui.c
@@ -11,14 +11,25 @@ void printFormattedMatrix(int **matrix, int dimension)
 	int i, j;
 
 	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); 
-	
+
 	SetConsoleTextAttribute(consoleHandle, 215);
 
 	for (i=0; i<dimension; i++, printf("\n") )
 		for(j=0; j<dimension; j++)
 		{
-			printf("%d", matrix[i][j]);
+
+			if(matrix[i][j] == 0)
+			{
+				SetConsoleTextAttribute(consoleHandle, 255);
+				printf("%d", matrix[i][j]);
+				SetConsoleTextAttribute(consoleHandle, 215);
+
+			} else printf("%d", matrix[i][j]);
 
 		}
+
+
+
+
 
 }
