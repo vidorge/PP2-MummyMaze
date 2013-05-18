4c184566118ffb1c8d99bbbbdf3cfa2728b049d3
Di-mi <ixydimitrov@gmail.com>
2013-05-18 03:53:00 +0200

    Prioritetni red i manhhatan

---
+++ MummyMaze/maze_solve.c
@@ -0,0 +1,47 @@
+#include "maze_solve.h"
+#include <math.h>
+#include <stdlib.h>
+int manhattanLength (int x1, int y1 , int x2, int y2 ){
+	return abs(x2-x1)+abs(y2-y1);
+}
+
+elemTree_t* deletePrioQueue(elemPrioQueue_t **q){
+	elemPrioQueue_t* temp;
+	elemTree_t* temp2;
+	if (q==null) 
+		return null; else {
+		temp=*q;
+		*q=temp->succ;
+		temp2= temp->info;
+		free(temp);
+		return temp2;
+	}
+}
+void insertPrioQueue(elemPrioQueue_t **q, elemTree_t* t, int len){
+	elemPrioQueue_t * new1;
+	int tmp1;
+	elemTree_t *tmp2;
+	new1= (elemPrioQueue_t*) malloc(sizeof(elemPrioQueue_t));
+	new1->menLen=len;
+	new1->info=t;
+	new1->succ=*q;
+	*q=new1;
+
+	while (new1!= null && new1->menLen > new1->succ->menLen){
+		tmp1=new1->succ->menLen;
+		tmp2=new1->succ->info;
+
+		new1->succ->menLen=new1->menLen;
+		new1->succ->info=new1->info;
+
+		new1->menLen=tmp1;
+		new1->info=tmp2;
+		
+		new1=new1->succ;
+
+	}
+
+
+
+
+}
--- MummyMaze/maze_solve.h
+++ MummyMaze/maze_solve.h
@@ -3,25 +3,34 @@
 
 
 /* makrooi za proveru statsa clanova glavne matrice */
-
-#define jeZid(char A ) ( A & 0x01)
-#define jeHint (char A )	(A & 0x02)
-#define jeCovek(char A ) (A & 0x04)
-
-int menhetenDuzina (int kordinataX1, int kordinataY1 , int kordinataX2, int kordinataY2 );
-typedef struct{
-	elemStablo **nizElem; 
+#define null 0
+/*#define isWall(char A ) ( A & 0x01)
+#define isHint (char A )	(A & 0x02)
+#define isPlayer(char A ) (A & 0x04)
+*/
+typedef struct elemTree_t
+{
 	char status;
 	int i,j;
-} elemStablo; 
+	struct elemTree_t *pred;
+	struct  elemTree_t **arrayElem; 
+	
+} elemTree_t; 
 
-typedef struct{
-	int	menDuz;
-	elemStablo *info;
-	elemPrioRed *sled;
+typedef struct elemPrioQueue_t
+{
+	int	menLen;
+	struct elemTree_t *info;
+	struct elemPrioQueue_t* succ;
 
 
-} elemPrioRed;
+} elemPrioQueue_t;
+int manhattanLength (int cordinateX1, int cordinateY1 , int cordinateX2, int cordinateY2 );
+
+
+elemTree_t* deletePrioQueue(elemPrioQueue_t** Q);
+void insertPrioQueue(elemPrioQueue_t **Q, elemTree_t* T,int Len);
+
 
 
 #endif
