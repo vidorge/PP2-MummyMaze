ca24f1b5faa5963d9aeba0010a8b233068fc6979
Di-mi <ixydimitrov@gmail.com>
2013-05-19 20:28:17 +0200

    B&B bez dealokacije

--- MummyMaze/maze_solve.c
+++ MummyMaze/maze_solve.c
@@ -1,4 +1,5 @@
 #include "maze_solve.h"
+#include "maze_create.h"
 #include <math.h>
 #include <stdlib.h>
 int manhattanLength (int x1, int y1 , int x2, int y2 ){
@@ -21,7 +22,7 @@ void insertPrioQueue(elemPrioQueue_t **q, elemTree_t* t, int len){
 	elemPrioQueue_t * new1;
 	int tmp1;
 	elemTree_t *tmp2;
-	new1= (elemPrioQueue_t*) malloc(sizeof(elemPrioQueue_t));
+	new1= (elemPrioQueue_t*) malloc(sizeof(elemPrioQueue_t)); 
 	new1->menLen=len;
 	new1->info=t;
 	new1->succ=*q;
@@ -45,13 +46,85 @@ void insertPrioQueue(elemPrioQueue_t **q, elemTree_t* t, int len){
 
 
 }
-/*elemTree_t*  branchAndBound(int ***matrix, int i1, int j1,int i2, int j2){
+int neighbours(int **matrix,char **visited,int i , int j){
+
+	char re;
+	re=0;
+	if(matrix[i+1][j] == 0 && visited[i-1][j]==0 )re+=4;
+	 if(matrix[i-1][j] == 0 && visited[i+1][j]==0)re+=1;
+	 if( matrix[i][j-1] == 0 && visited[i][j-1]==0)re+=8;
+	 if( matrix[i][j+1] == 0 && visited[i][j+1]==0) re+=2;
+	 return re;
+	 
+}
+elemTree_t* createNode(int i, int j,elemTree_t* pre){
+	elemTree_t* re;
+
+	re=malloc (sizeof(elemTree_t));
+	re->status=0;
+	re->pred=pre;
+	re->i=i;
+	re->j=j;
+	re->arrayElem =null;
+
+	return re;
+
+}
+
+elemTree_t*  branchAndBound(int **matrix, int i1, int j1,int i2, int j2,dimension_t dimension){
+	int i;
+	int nb;
 	elemPrioQueue_t *queue=null;
+	elemTree_t *koren, *tmp;
+								//ne zaboravi dealokaciju
+	
+	char **visited=malloc (sizeof(char*));
+	
+	for (i=0;i<dimension.x; i++)
+		visited[i]=calloc(dimension.y,sizeof(char));
+	visited[i1][j1]=1;
 
-	*/
-	// od  matrix[i][j] knapraviti  koren stabla 
+	koren= createNode(i1,j1,null);
 
-	//ubaciti u red pomocu insertPrioQueue()
+	insertPrioQueue(&queue,koren,manhattanLength( i1, j1,i2, j2));
+	tmp = deletePrioQueue(&queue);
+	while(tmp->i!=i2 && tmp->j!=j2){
+		nb=neighbours(matrix,visited,tmp->i,tmp->j);
+		tmp->arrayElem= malloc(sizeof(elemTree_t*));
+		for(i=0;i<sumBits(nb)+1;i++) 
+			tmp->arrayElem[i]=malloc(sizeof(elemTree_t));
+		i=0;
+		
+		if(isUp(nb)) {
+		tmp->arrayElem[i]=createNode(tmp->i+1,tmp->j,tmp);
+		insertPrioQueue(&queue,tmp->arrayElem[i++],manhattanLength(tmp->i+1,tmp->j,i2, j2));
+		}
+		if(isRight(nb)) {
+		tmp->arrayElem[i]=createNode(tmp->i,tmp->j+1,tmp);
+		insertPrioQueue(&queue,tmp->arrayElem[i++],manhattanLength(tmp->i,tmp->j+1,i2, j2));
+		}
+		if(isDown(nb)) {
+		tmp->arrayElem[i]=createNode(tmp->i-1,tmp->j,tmp);
+		insertPrioQueue(&queue,tmp->arrayElem[i++],manhattanLength(tmp->i-1,tmp->j,i2, j2));
+		}
+		if(isLeft(nb)) {
+		tmp->arrayElem[i]=createNode(tmp->i,tmp->j-1,tmp);
+		insertPrioQueue(&queue,tmp->arrayElem[i++],manhattanLength(tmp->i,tmp->j-1,i2, j2));
+		}
+		tmp->arrayElem[i]=null;
+		
+		tmp=deletePrioQueue(&queue);
+		
+	}
+
+
+
+
+	
+	
 
 	//poceti petlju
-//}
+
+	return koren;
+
+}
--- MummyMaze/maze_solve.h
+++ MummyMaze/maze_solve.h
@@ -1,13 +1,14 @@
 #ifndef _maze_solve_h
 #define _maze_solve_h
-
+#include "maze_create.h"
 
 /* makrooi za proveru statsa clanova glavne matrice */
 #define null 0
-/*#define isWall(char A ) ( A & 0x01)
-#define isHint (char A )	(A & 0x02)
-#define isPlayer(char A ) (A & 0x04)
-*/
+#define isUp(A) (A & 0x01)
+#define isRight(A) (A & 0x02)
+#define isDown(A) (A & 0x04)
+#define isLeft(A) (A & 0x08)
+#define sumBits(A) (A&0x01+ A & 0x02+ A & 0x04+A & 0x08 )
 typedef struct elemTree_t
 {
 	char status;
@@ -30,7 +31,7 @@ int manhattanLength (int cordinateX1, int cordinateY1 , int cordinateX2, int cor
 
 elemTree_t* deletePrioQueue(elemPrioQueue_t** Q);
 void insertPrioQueue(elemPrioQueue_t **Q, elemTree_t* T,int Len);
-elemTree_t*  branchAndBound(int ***matrix, int i, int j,int, int);
-
-
+elemTree_t*  branchAndBound(int **matrix, int i, int j,int, int,dimension_t dimension);
+elemTree_t* createNode(int i, int j,elemTree_t* pre);
+int neighbours(int **matrix,char **visted,int i , int j);
 #endif
